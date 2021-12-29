4cd60467a3ef502c3e6cc2bc5b11b730de7976b8
Burak Ozdemir <ozdemirburak1912@gmail.com>
2021-12-29 10:49:01 +0300

    register

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
+import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import "antd/dist/antd.css";
+import App from "./App";
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'antd/dist/antd.css';
-
-ReactDOM.render( <App /> ,document.getElementById('root'));
-
+const store = createStore(rootReducer, applyMiddleware(thunk));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
